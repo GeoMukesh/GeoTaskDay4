40fb8b9de84b03bd84f6ae88ea064ebe1feafe2a
GeoMukesh <geomukesh777@gmail.com>
2022-04-22 22:27:01 +0530

    new project added

---
+++ src/org/test/A.java
@@ -0,0 +1,21 @@
+package org.test;
+
+	
+	
+public class A extends B{
+	  public A() {
+
+	    System.out.println("Default const...");
+	  }
+
+	  public static void main(String[] args) {
+	    A a = new A();
+	  }
+		}
+
+		
+		
+
+
+
+
---
+++ src/org/test/B.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class B {
+
+
+	 public B() {
+		    System.out.println("Super class");
+		  }
+
+		  public B(int id) {
+		    System.out.println(id);
+		  }
+}
